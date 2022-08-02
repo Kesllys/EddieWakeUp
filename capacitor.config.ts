aaf639a3a5122e69035406d2c25a01e93e79e5de
Ana Carolina Antonio de Sousa <kellycristinacardoso.713@gmail.com>
2022-08-01 21:24:24 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sidemenuApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
